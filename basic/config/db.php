a3de7f1e4529315da3c3aea660db1dc09fca4396
Ángel <aangelfran@gmail.com>
2023-01-06 11:59:14 +0100

    Subida del proyecto

---
+++ basic/config/db.php
@@ -0,0 +1,11 @@
+<?php
+//Descomenta la línea que se corresponde con tu proyecto de 
+//trabajo y comenta la línea que carga "db_orig.php".
+return require( 'db_orig.php');
+//return require( 'db_proyecto_01_comenta_libros.php');
+//return require( 'db_proyecto_02_establecimientos_hosteleros.php');
+?>
+<pre>
+Descomenta la línea que se corresponde con tu proyecto de trabajo en el archivo de configuración de la base de datos.
+<?php echo __FILE__; throw new Exception(); ?>
+</pre>
