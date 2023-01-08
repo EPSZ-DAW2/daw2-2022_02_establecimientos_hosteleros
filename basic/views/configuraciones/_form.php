8dacfd72816da363e1c637e06370aa4a86fd5b93
Ángel <aangelfran@gmail.com>
2023-01-08 11:31:13 +0100

    F1 - Inicio mantenimiento configuraciones

---
+++ basic/views/configuraciones/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Configuracion $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="configuracion-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'variable')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'valor')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
