7854c2995d688335277419e102bc80332c32cb62
Saul-prog <sauloterogarcia54@gmail.com>
2023-01-22 21:16:16 +0100

    Se puede hacer búsquedas por zonas

--- basic/controllers/LocalController.php
+++ basic/controllers/LocalController.php
@@ -29,21 +29,30 @@ class LocalController extends \yii\web\Controller
 	}
 
 	//Acción inicial de la web, muestra un listado de locales
-    public function actionIndex($id=null)
+    public function actionIndex($id=null,$zona=null)
     {
 		$locales=Local::find()->where(['visible'=>1]);
+
+        //$zona= (isset($_GET['zona']) ? $_GET['zona'] : NULL);
+        //$zona = Yii::$app->request->get('zona');
 
         //Cuando se cree un filtro de locales añadir en la variable $filtro ese filtro
 		$filtro='prioridad';
         //Cuando las zonas sean funcionales descomentar las siguientes lineas y comentar la linea marcada
         //Al descomentar la siguiente linea añadir a esa variable la zona filtro
-        //$filtro_zona='zona';
-        //$localespat=Local::find()->where(['visible'=>1,'zona'=>$filtro_zona])->orderBy([$filtro=>SORT_ASC])->limit(5);
-        //Si se descomentan las anteriores comentar la siguiente
-		$localespat=Local::find()->where(['visible'=>1])->orderBy([$filtro=>SORT_ASC])->limit(5);
+        $filtro_zona=$zona;
+        if($filtro_zona!=null){
+            $localespat=Local::find()->where(['visible'=>1,'zona_id'=>$filtro_zona])->orderBy([$filtro=>SORT_ASC])->limit(5);
+        }else{
+            $localespat=Local::find()->where(['visible'=>1])->orderBy([$filtro=>SORT_ASC])->limit(5);
+        }
+
 
 		if(isset($id) && $id!=null)
 			$locales->andWhere(['hostelero_id'=>$id]);
+        if(isset($zona)&& $zona!=null){
+            $locales->andWhere(['zona_id'=>$zona]);
+        }
 
 		return $this->render('index', [
 			'locales'=>$locales->all(),
